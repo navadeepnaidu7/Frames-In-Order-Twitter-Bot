44fafbbdb8ad1140221954cd8022eaf468a63b36
Navadeep Naidu <84763359+navadeepnaidu7@users.noreply.github.com>
2024-01-07 18:17:21 +0530

    Changed the code to tweet from google drive

--- tweet.py
+++ tweet.py
@@ -1,6 +1,11 @@
 import tweepy
 import os
 import random
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from oauth2client.client import OAuth2Credentials
+import requests
+from io import BytesIO
 
 # Authenticate using environment variables
 bearer_token = os.environ.get("BEARER_TOKEN")
@@ -22,20 +27,36 @@ client = tweepy.Client(
     wait_on_rate_limit=True,
 )
 
-# Determine image folder relative to the script's location
-script_dir = os.path.dirname(__file__)
-image_folder = os.path.join(script_dir, "images")  # Assuming images are in the "images" subfolder
 
-image_files = [f for f in os.listdir(image_folder) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+# Google Drive authentication
+gauth = GoogleAuth()
+gauth.credentials = OAuth2Credentials(
+    access_token=None,  # set access_token to None
+    client_id=os.environ.get("CLIENT_ID"),  # these values are obtained from the "cloud console"
+    client_secret=os.environ.get("CLIENT_SECRET"),  # these values are obtained from the "cloud console"
+    refresh_token=os.environ.get("REFRESH_TOKEN"),  # obtained by running the auth flow on a local machine
+    token_expiry=None,
+    token_uri='https://accounts.google.com/o/oauth2/token',  # Google's token URI
+    user_agent='pyDrive',  # you can set this to any string
+    revoke_uri='https://accounts.google.com/o/oauth2/revoke'  # Google's revoke URI
+)
+drive = GoogleDrive(gauth)
 
-# Tweet with each image, caption, and random hashtag
-for image_file in image_files:
-    image_path = os.path.join(image_folder, image_file)
+# Specify the Google Drive folder ID
+folder_id = '1L0MJqJx-qirE9K1Sdy2W2SyYWNo_wY_Q'  # Replace with your folder ID
 
-    media = api.media_upload(image_path)
+# Retrieve photo files from Google Drive
+file_list = drive.ListFile({'q': f"'{folder_id}' in parents and trashed=false"}).GetList()
+
+for file in file_list:
+    # Get the image file from Google Drive as a file-like object
+    response = requests.get(file['webContentLink'])
+    image_file = BytesIO(response.content)
+
+    # Upload the image to Twitter
+    media = api.media_upload(filename=file['title'], file=image_file)
 
     caption = f"test tweet with image! {random.choice(['#JrNTR', '#Devara', '#Bot'])}"
-
     tweet = client.create_tweet(text=caption, media_ids=[media.media_id])
 
-    print(f"Tweeted image: {image_file}")
+    print(f"Tweeted image: {file['title']}")
