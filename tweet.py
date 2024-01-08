f5c01c3118da3ae1f10b455393ec85643ddc14be
Navadeep Naidu <84763359+navadeepnaidu7@users.noreply.github.com>
2024-01-08 12:47:28 +0530

    minor changes

--- tweet.py
+++ tweet.py
@@ -67,7 +67,7 @@ image_file = BytesIO(response.content)
 media = api.media_upload(filename=file['title'], file=image_file)
 
 # Create the caption
-caption = f"#DevaraGlimpse - tweet {photo_index} of {len(file_list)}"
+caption = f"#DevaraGlimpse - Frame {photo_index} of {len(file_list)} Frames."
 
 # Tweet with the image and caption
 tweet = client.create_tweet(text=caption, media_ids=[media.media_id])
